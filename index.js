87fc795a6a88a653477f4b855f64357cb086edf4
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-14 03:39:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,3 +28,12 @@ function makeInt(n){
 function makeInt(n){
   return parseInt (n, 10);
 }
+
+function preserveDecimal (n){
+  return parseInt (n, 10)
+}
+
+function preserveDecimal (n){
+  return parseInt ('n', 10)
+}
+
